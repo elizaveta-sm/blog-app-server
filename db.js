589dde6809feaceb196e5c02822a7ec53c123785
Elizaveta S <elizaveta.s.webdev@gmail.com>
2024-03-15 13:12:24 +0300

    changed the pool

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const Pool = require('pg').Pool;
 require('dotenv').config() // for sensitive information
 
 const pool = new Pool({
-    host: process.env.HOST,
+    host: import.meta.env.VITE_HOST,
     port: 5432,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
+    user: import.meta.env.VITE_PGUSER,
+    password: import.meta.env.VITE_PGPASSWORD,
     database: 'postgres'
 }) // how we communicate with the db
 
