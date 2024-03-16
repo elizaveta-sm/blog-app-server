24ecf8953d735b5726ea30861422d5afb49f3467
Elizaveta S <elizaveta.s.webdev@gmail.com>
2024-03-16 14:29:30 +0300

    changed the host

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 require('dotenv').config() // for sensitive information
 
 const pool = new Pool({
-    host: process.env.POSTGRES_HOST,
+    host: 'aws-0-eu-central-1.pooler.supabase.com',
     port: 5432,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
